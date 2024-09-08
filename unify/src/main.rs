0f953acaf8f4416d14fb41accf726b7281f2a85c
you <you@you.com>
2024-09-07 22:52:07 -0500

    first phase, already got the common files

--- unify/src/main.rs
+++ unify/src/main.rs
@@ -20,19 +20,24 @@
 //------------------------------------------------------------------
 use std::io;
 use std::fs::{self};
+use std::fs::File;
+
 use std::path::Path;
 use std::collections::HashSet;
 
-fn get_files(dir: &Path, v: &mut Vec<String>) -> io::Result<()> {
+fn get_files(dir: &Path, v: &mut HashSet<String>) -> io::Result<()> {
     if dir.is_dir(){
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
             if path.is_dir(){
-                get_files(&path, v);
+                match get_files(&path, v) {
+                    Ok(_) => println!("Succesfully worked!"),
+                    Err(why) => println!("The error is: {}", why),
+                };
             }else {
                 // println!("{}", path.display());
-                v.push(String::from(path.to_str().unwrap()));                
+                v.insert(String::from(path.to_str().unwrap()));                
             }
         }
     }
@@ -49,15 +54,71 @@ fn get_files(dir: &Path, v: &mut Vec<String>) -> io::Result<()> {
         }
     */
 }
+fn remove_prefix_in_place(set: &mut HashSet<String>, prefix: &str){
+    let new_set : HashSet<String> = set.iter().map(|entry| {
+        if entry.starts_with(prefix) {
+            entry.strip_prefix(prefix).unwrap_or(entry).to_string()
+        } else{
+            entry.to_string()
+        }
+    }).collect();
+    
+    set.clear();
+    
+    set.extend(new_set);
+
+}
+
+fn get_filesize(path: &String, prefix: &str) -> std::io::Result<u64>{
+    let complete_path = prefix.to_string() + path;
+    let fpath = Path::new(&complete_path);
+    let f = File::open(fpath)?;
+    let metadata = f.metadata()?;
+    // println!("{:#?}", metadata);
+    Ok(metadata.len())    
+}
+
+// fn get_complete_path(prefix:&str, file:&str) -> String{
+//     String::from(prefix + file)
+// }
+
+fn do_versions(common_files: &HashSet<String>, prefix_r1: &str, prefix_r2: &str)  {
+    for file in common_files {
+        let file_size_r1 = match get_filesize(&file, &prefix_r1){
+            Err(why) => panic!("Error why: {}", why),
+            Ok(file_size) => file_size,
+        };
+        let file_size_r2 = match get_filesize(&file, &prefix_r2){
+            Err(why) => panic!("Error why: {}", why),
+            Ok(file_size) => file_size, 
+        };
+        
+        if file_size_r1 == file_size_r2{
+            println!("Are the same");
+            //move the file to root3
+            println!("Movinf the file .........");
+            
+        }else{
+            println!("You need versioning!");
+            //change their names to -v1 -v2 and move them
+        }
+
+        // println!("file_r1 size = {}, file_r2 size= {}",file_size_r1, file_size_r2 );
+    }
+
+    // Ok(())
+}
+fn move_file() {
+
+}
+
 
 fn main() -> io::Result<()> {
-    let mut r1: Vec<String> = Vec::new();
-    let mut r2: Vec<String> = Vec::new();
+    let mut r1: HashSet<String> = HashSet::new();
+    let mut r2: HashSet<String> = HashSet::new();
 
-    
-
-    let rpath_r1 = Path::new("../test/r2");
-    let rpath_r2 = Path::new("../test/r1");
+    let rpath_r1 = Path::new("../test/r1");
+    let rpath_r2 = Path::new("../test/r2");
 
     match get_files(&rpath_r1, &mut r1) {
         Ok(_) => println!("Succesfully worked!"),
@@ -67,14 +128,41 @@ fn main() -> io::Result<()> {
         Ok(_) => println!("Succesfully worked!"),
         Err(why) => println!("The error is: {}", why),
     };
+    let prefix_r1 = "../test/r1";
+    let prefix_r2 = "../test/r2";
+
+    // println!("R1");
+    // for f in &r1{
+    //     println!("{}",f);
+    // }
+    // println!("R2");
+    // for f in &r2{
+    //     println!("{}",f);
+    // }
+    remove_prefix_in_place(&mut r1, &prefix_r1);
+    remove_prefix_in_place(&mut r2, &prefix_r2);
     println!("R1");
-    for f in r1{
+    for f in &r1{
         println!("{}",f);
     }
     println!("R2");
-    for f in r2{
+    for f in &r2{
         println!("{}",f);
     }
 
+    let common: HashSet<_> = r1.intersection(&r2).cloned().collect();
+    println!("Common files are: {:?}", common);
+    let dif1: HashSet<_> = r1.difference(&r2).cloned().collect();
+    println!("r1 - r2: {:?}", dif1);
+    let dif2: HashSet<_> = r2.difference(&r1).cloned().collect();
+    println!("r2 - r1: {:?}", dif2);
+
+    do_versions(&common, &prefix_r1, &prefix_r2);
+
     Ok(())
 }
+
+
+
+
+
