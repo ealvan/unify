14fc6a99f8f81208c28ec80b3c2eaeb32f19758f
you <you@you.com>
2024-11-07 07:03:53 -0500

    feat: completing girst phase of the plan

--- unify/src/main.rs
+++ unify/src/main.rs
@@ -1,4 +1,5 @@
 #[warn(unused_variables)]
+
 use std::io;
 use std::fs::{self, OpenOptions};
 use std::fs::File;
@@ -8,6 +9,7 @@ use fern::Dispatch;
 use std::collections::HashSet;
 use std::path::Path;
 use std::io::Write;
+use itertools::Itertools;
 
 fn get_files(dir: &Path, v: &mut Vec<String>) -> io::Result<()> {
     for entry in fs::read_dir(dir)? {
@@ -30,8 +32,20 @@ fn print_vector(rootn: &Vec<String>){
         println!("{}",f);
     }
 }
-fn write_rootn_files(rootn:&Vec<String>, root_log_name: &str) -> std::io::Result<()>{
-    let mut content: String = rootn.join("\n");
+// fn write_rootn_files(rootn:&Vec<String>, root_log_name: &str) -> std::io::Result<()>{
+//     let mut content: String = rootn.join("\n");
+//     let mut f = OpenOptions::new()
+//         .write(true)
+//         .append(true)
+//         .create(true)
+//         .open(root_log_name)
+//         .expect("error opening a file");
+
+//     f.write_all(content.as_bytes()).expect("Unable to write");
+//     Ok(())
+// }
+fn write_rootn_files(rootn:&HashSet<String>, root_log_name: &str) -> std::io::Result<()>{
+    let mut content: String = itertools::join(rootn, "\n");
     let mut f = OpenOptions::new()
         .write(true)
         .append(true)
@@ -42,6 +56,13 @@ fn write_rootn_files(rootn:&Vec<String>, root_log_name: &str) -> std::io::Result
     f.write_all(content.as_bytes()).expect("Unable to write");
     Ok(())
 }
+
+fn remove_prefix_in_place(myvec: &mut Vec<String>, prefix: &str){
+    *myvec = myvec.iter()
+              .filter_map(|entry| entry.strip_prefix(prefix).map(|e| e.to_string()))
+              .collect();
+}
+
 fn main(){
     println!("FIRST PHASE");
     //define root1, root2 & root3
@@ -66,9 +87,30 @@ fn main(){
     print_vector(&fr2);
 
     //Now write the files in a persistent .log file
-    let root1_logname = "root1_file.log";
-    let root2_logname = "root2_file.log";
-    write_rootn_files(&fr1, &root1_logname);
-    write_rootn_files(&fr2, &root2_logname);
+    // let root1_logname = "root1_file.log";
+    // let root2_logname = "root2_file.log";
+    let mut prefix = root1.display().to_string();    
+    remove_prefix_in_place(&mut fr1, &prefix);
+    prefix = root2.display().to_string();
+    remove_prefix_in_place(&mut fr2, &prefix);
+
+    // write_rootn_files(&fr1, &root1_logname);
+    // write_rootn_files(&fr2, &root2_logname);
+
+    let root1_set: HashSet<_> = fr1.into_iter().collect();
+    let root2_set: HashSet<_> = fr2.into_iter().collect();
+
+    let inter = "root1_and_root2.log";
+    let r1r2 = "root1-root2.log";
+    let r2r1 = "root2-root1.log";
+
+    let common: HashSet<_> = root1_set.intersection(&root2_set).cloned().collect();
+    write_rootn_files(&common, &inter);
+    let dif1: HashSet<_> = root1_set.difference(&root2_set).cloned().collect();
+    write_rootn_files(&dif1, &r1r2);
+    let dif2: HashSet<_> = root2_set.difference(&root1_set).cloned().collect();
+    write_rootn_files(&dif2, &r2r1);
+
+
 
 }
